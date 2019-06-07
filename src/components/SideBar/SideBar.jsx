989c41e5b5d3e20567e51d79c7fb8b4b9ca8c68f
José Osorno <the_videogame_master@hotmail.com>
2019-06-06 23:55:21 -0500

    Added filter logic

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,50 @@
+//Core imports
+import React from "react";
+
+//Components
+
+//Styling imports
+import "./SideBar.scss";
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ""
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+    this.props.onSearch(event.target.value);
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div className="side-bar">
+        <div className="side-bar__title-container">
+          <div className="side-bar__title-container--value" />
+        </div>
+        <div className="side-bar__search-container">
+          <div className="side-bar__search-container--input">
+            <i className="fas fa-search" />
+            <input
+              className="side-bar__search-container--input-element"
+              type="text"
+              name="Search"
+              value={this.state.searchTerm}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="side-bar__content-container">{children}</div>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
--- src/views/UsersView/UsersView.jsx
+++ src/views/UsersView/UsersView.jsx
@@ -1,22 +1,47 @@
+//Core imports
 import React from "react";
 
 import UserCard from "components/UserCard";
+import SideBar from "components/SideBar";
 
 import "./UsersView.scss";
 
 import users from "users";
 
 class UsersView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { data: users };
+  }
   renderUsers = () => {
-    return users.map(user => {
+    return this.state.data.map(user => {
       return <UserCard user={user} />;
     });
+  };
+
+  filterUsers = searchTerm => {
+    if (searchTerm === "") {
+      this.setState({ data: users });
+    } else {
+      const result = users.filter(user => {
+        return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      this.setState({ data: result });
+    }
   };
 
   render() {
     return (
       <section id="users-view">
-        <div className="users-view__users-grid">{this.renderUsers()}</div>
+        <div>
+          <div className="users-view__side-bar">
+            <SideBar onSearch={this.filterUsers}>
+              esta vista es una pistola
+            </SideBar>
+          </div>
+          <div className="users-view__users-grid">{this.renderUsers()}</div>
+        </div>
       </section>
     );
   }
