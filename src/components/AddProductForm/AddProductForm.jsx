4c0a32df8ab95c29f078537ba796c11d30a5b09f
Eduardo Del Aguila <edelaguilam@outlook.com>
2019-07-15 09:52:31 -0500

    Add user to DB
    WIP: Add user
    -Image upload to firebase
    -Service
    -Add User form

--- src/components/AddProductForm/AddProductForm.jsx
+++ src/components/AddProductForm/AddProductForm.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import firestore from 'service/firestore';
 
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import { DropzoneArea } from 'material-ui-dropzone'
 
 import './AddProductForm.scss';
 
@@ -16,7 +15,9 @@ export default class AddProductForm extends Component {
     this.state = {
       name: '',
       price: 0,
-
+      vendor: '',
+      description: '',
+      image: null,
     }
   }
 
@@ -25,12 +26,31 @@ export default class AddProductForm extends Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  handleFileUpload = (file) => {
+    console.log(file);
+    let myBlob = new Blob(file, { type: file[0].type });
+    let myReader = new FileReader();
+    //handler executed once reading(blob content referenced to a variable) from blob is finished. 
+    myReader.addEventListener("loadend", (result) => {
+      console.log(result.srcElement.result);
+      this.setState({ image: result.srcElement.result })
+    });
+    //start the reading process.
+    myReader.readAsText(myBlob);
+
+  }
+
+  onAddProduct = () => {
+    const { description, image, name, price, vendor } = this.state;
+    firestore.addProduct({ description, image, name, price, vendor });
+  }
+
   render() {
-    const { name, price } = this.state;
+    const { name, price, vendor, description } = this.state;
     return (
       <Card className="mla-add-product-form">
         <TextField
-          id="standard-name"
+          className="col-sm-12 col-gl-4"
           label="Nombre"
           value={name}
           name="name"
@@ -38,7 +58,7 @@ export default class AddProductForm extends Component {
           margin="normal"
         />
         <TextField
-          id="standard-name"
+          className="col-sm-12 col-gl-4"
           label="Precio"
           value={price}
           name="price"
@@ -46,16 +66,42 @@ export default class AddProductForm extends Component {
           margin="normal"
           type="number"
           min="1"
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
         />
-        <Select
-          name="Vendor"
-        >
-          <MenuItem value="" />
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
+        <TextField
+          className="col-sm-12 col-gl-4"
+          label="Marca"
+          value={vendor}
+          name="vendor"
+          onChange={this.handleInputs}
+          margin="normal"
+          type="text"
+        />
+
+        <TextField
+          className="col-12"
+          label="Descripción"
+          value={description}
+          name="description"
+          onChange={this.handleInputs}
+          margin="normal"
+          type="text"
+        />
+
+        <span className="mla-add-product-form__dropzone-title">Imagen:</span>
+        <DropzoneArea
+          onChange={this.handleFileUpload}
+          acceptedFiles={["image/*"]}
+          filesLimit={1}
+          maxFileSize={10000000}
+          dropzoneClass="mla-add-product-form__dropzone"
+          showPreviews={true}
+          dropzoneText=""
+          showPreviewsInDropzone={false}
+        />
+
+        <Button variant="contained" color="primary" onClick={this.onAddProduct}>
+          Registrar
+          </Button>
       </Card>
     )
   }
